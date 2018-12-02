ae4a276191e5d929fc2363f1fdbc7ae51a872872
Jiaming Bai <jiamingbai@MacBook-Pro-2.local>
2018-12-02 13:45:30 -0500

    test python php

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, Response, request, redirect, url_for
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('predict.html')
+
+@app.route("/forward/", methods=['POST'])
+def predict():
+    #Moving forward code
+    forward_message = "Testing..."
+    return render_template('predict.html', message=forward_message);
